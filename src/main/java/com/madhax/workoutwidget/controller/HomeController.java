fedf66af8b0409cf1a82fe1a31b96992215ba887
James Cathcart <cathcart.jm@gmail.com>
2019-03-20 09:12:24 -0600

    Chaged request mapping annotation to GetMapping

--- src/main/java/com/madhax/workoutwidget/controller/HomeController.java
+++ src/main/java/com/madhax/workoutwidget/controller/HomeController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping({"", "/", "index", "index.html"})
 public class HomeController {
 
-    @RequestMapping({"", "/", "index", "index.html"})
+    @GetMapping
     public String showIndexPage() {
         return "index";
     }
